7b870023ea061021fbb371944845dbf9cffa2862
NewSha <369189453@qq.com>
2018-01-21 22:24:57 +0800

    2018-1-21

---
+++ Chapter2/2.py
@@ -0,0 +1,53 @@
+#第二章的练习
+
+#2-1
+message = "今天开始重新学习Python！"
+print(message)
+
+#2-2
+message = "第一条消息！"
+print(message)
+message = "第二条信息！"
+print(message)
+
+#2-3
+name = "shayaxiong"
+print("Hello " + name.title() + ", would you like to learn some Python today?")
+
+#2-4
+name = "shayaxiong"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+#2-5
+company = "nike"
+ad = "“everything is possible!”"
+print(company + "'s ad is " + ad)
+
+#2-6
+famous_person = "nike"
+message = famous_person + "'s ad is " + "“everything is possible!”"
+print(message)
+
+#2-7
+name = "\t             shayaxiong              \t"
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+#2-8
+print(2 + 6)
+print(10 - 2)
+print(2 * 4)
+print(24 / 3)
+
+#2-9
+num = 2
+print("my favorite num is " + str(num))
+
+#2-10
+#这是一个注释
+
+#2-11
+import this
